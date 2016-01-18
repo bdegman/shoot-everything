0a9fa05b7123dbd344c6ba048002bb3a28ae1b06
Brian Degman <bdegman@sooloos.com>
2016-01-17 23:15:29 -0600

    start of wave framework

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,12 +5,18 @@ public class GameController : MonoBehaviour {
 
 	public GameObject hazard;
 	public GameObject enemy;
-	public Vector3 spawnValues;
+	public float spawnMinX;
+	public float spawnMaxX;
+	public float spawnY;
+
 	public int hazardCount;
 	public int enemyCount;
+
 	public float spawnWait;
 	public float startWait;
 	public float waveWait;
+
+	private int waveCount = 1;
 
 	void Start () {
 		StartCoroutine (SpawnWaves ());
@@ -21,15 +27,21 @@ public class GameController : MonoBehaviour {
 		while (true) {
 			for (int i = 0; i < hazardCount; i++) {
 				if (i < enemyCount) {
-					Vector3 enemySpawnPosition = new Vector3 (spawnValues.x, Random.Range (-spawnValues.y, spawnValues.y), spawnValues.z);
+					Vector3 enemySpawnPosition = new Vector3 (Random.Range (spawnMinX, spawnMaxX), 4, 0);
 					Quaternion enemySpawnRotation = Quaternion.identity;
 					Instantiate (enemy, enemySpawnPosition, enemySpawnRotation);
 				}
-				Vector3 spawnPosition = new Vector3 (spawnValues.x, Random.Range (-spawnValues.y, spawnValues.y), spawnValues.z);
+				float rnd = Random.Range (-1, 1);
+				float direction = Mathf.Sign (rnd);
+				Vector3 spawnPosition = new Vector3 (Random.Range (spawnMinX, spawnMaxX) * direction, spawnY, 0);
 				Quaternion spawnRotation = Quaternion.identity;
 				Instantiate (hazard, spawnPosition, spawnRotation);
 				yield return new WaitForSeconds (spawnWait);
 			}
+			enemyCount = enemyCount + waveCount;
+			hazardCount = hazardCount * waveCount;
+
+			waveCount++;
 			yield return new WaitForSeconds (waveWait);
 		}
 
--- Assets/Scripts/HazardMover.cs
+++ Assets/Scripts/HazardMover.cs
@@ -3,11 +3,19 @@ using System.Collections;
 
 public class HazardMover : MonoBehaviour {
 
+	private GameObject player;
 	private Rigidbody2D rb2d;
+
+	public float direction;
 	public float speed;
 
 	void Start () {
+		player = GameObject.Find("player");
+
+		// check which side the player is on
+		direction = Mathf.Sign (transform.position.x - player.transform.position.x);
+
 		rb2d = GetComponent<Rigidbody2D>();
-		rb2d.velocity = -transform.right * speed;
+		rb2d.velocity = -transform.right * speed * direction;
 	}
 }
